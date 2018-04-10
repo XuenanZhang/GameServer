a86ce6e31e1577818baf0284e99151a2f6f2d2f8
zxn <xuenanzhang@163.com>
2018-04-10 16:59:25 +0800

    make修改

--- engine/src/build.mk
+++ engine/src/build.mk
@@ -2,7 +2,7 @@ ifndef GAME_CONFIG
 GAME_CONFIG := Debug
 endif
 
-
+VPATH += $(shell find $(CURDIR) -type d)
 LIBDIR = $(GAME_ROOT)/src/libs
 
 #执行文件路径
@@ -10,14 +10,14 @@ ifdef BIN
 MAKE_LIBS = 1
 OUTPUT_DIR = $(GAME_ROOT)/bin
 OUTPUT_FILE = $(OUTPUT_DIR)/$(BIN)
-OUTPUT_OBJDIR = $(GAME_ROOT)/src/objs/$(GAME_CONFIG)/$(BIN)
+OUTPUT_OBJDIR = $(GAME_ROOT)/objs/$(GAME_CONFIG)/$(BIN)
 endif # BIN
 
 #静态库路径
 ifdef LIB
 OUTPUT_DIR = $(LIBDIR)
 OUTPUT_FILE = $(OUTPUT_DIR)/lib$(LIB).a
-OUTPUT_OBJDIR = $(GAME_ROOT)/src/objs/$(GAME_CONFIG)/$(LIB)
+OUTPUT_OBJDIR = $(GAME_ROOT)/objs/$(GAME_CONFIG)/$(LIB)
 endif # LIB
 
 #共享库
@@ -27,7 +27,7 @@ endif
 
 #编译配置
 INCLUDES := $(GAME_ROOT)/src
-INCLUDES += $(filter-out, $(CURDIR), $(shell find $(CURDIR) -type d))
+INCLUDES += $(shell find $(CURDIR) -type d)
 INCLUDES += $(shell find $(GAME_ROOT)/src/lib -type d)
 
 #链接配置
@@ -69,7 +69,7 @@ CXXFLAGS += -Wall
 # -Werror时所有的警告都变成错误,使出现警告时也停止编译.需要和指定警告的参数一起使用.
 # CXXFLAGS += -Werror
 
-CXXFLAGS += -DGAME_CONFIG=\"${GAME_CONFIG}\"
+CXXFLAGS += -MMD -DGAME_CONFIG=\"${GAME_CONFIG}\"
 
 ifeq ($(GAME_CONFIG), Release)
 CXXFLAGS += -O3
@@ -107,6 +107,7 @@ LINK.cpp = $(CXX) $(CXXFLAGS) $(LDFLAGS)
 all: info $(OUTPUT_DIR) $(OUTPUT_OBJDIR) $(OUTPUT_FILE) done
 
 info:
+	@echo VPATH = $(VPATH)
 	@echo CURDIR = $(CURDIR)
 	@echo OUTPUT_FILE = $(OUTPUT_FILE)
 	@echo SRC_CPP = $(SRC_CPP)
@@ -127,8 +128,7 @@ done:
 # endif
 
 clean:
-	rm -rf $(OUTPUT_OBJDIR) $(OUTPUT_FILE)
-
+	@rm -rf $(OUTPUT_OBJDIR) $(OUTPUT_FILE)
 
 # DIRS := $(shell find . -type d ! -path "*.svn" ! -path "*.git")
 SRC_CPP := $(shell find $(CURDIR) -name "*.cpp")
@@ -142,13 +142,15 @@ OUTPUT_DEPS := $(OUTPUT_OBJS:%.o=%.d)
 ifdef MAKE_LIBS
 MY_LIBNAMES = $(foreach L, $(MY_LIBS), $(LIBDIR)/lib$(L).a)
 
-$(MY_LIBNAMES) : always
-	$(MAKE) -C $(GAME_ROOT)/src/lib/$(subst XXXXX,_lib,$(subst lib,,$(subst _lib,XXXXX,$(*F)))) GAME_CONFIG=$(GAME_CONFIG)
+# $(MY_LIBNAMES) : always
+	# $(MAKE) -C $(GAME_ROOT)/src/lib/$(subst XXXXX,_lib,$(subst lib,,$(subst _lib,XXXXX,$(*F)))) GAME_CONFIG=$(GAME_CONFIG)
 endif
 
 ifneq ($(SRC_CPP),)
--include OUTPUT_DEPS
+-include $(OUTPUT_DEPS)
+
 endif
+
 
 $(OUTPUT_DIR): 
 	@mkdir -p $(OUTPUT_DIR)
@@ -156,12 +158,14 @@ $(OUTPUT_DIR):
 $(OUTPUT_OBJDIR): 
 	@mkdir -p $(OUTPUT_OBJDIR)
 
-
+# /home/coder/GameServer/engine/objs/Debug/loginserver/main.o:/home/coder/GameServer/engine/src/server/loginserver/tt/pp.h
 $(OUTPUT_OBJDIR)/%.o: %.cpp
-	$(COMPILE.cpp) $< -o $@
+	@echo $+
+	$(COMPILE.cpp) $< $(OUTPUT_OPTION)
 	
 ifdef BIN
 $(OUTPUT_FILE):: $(OUTPUT_OBJS) $(MY_LIBNAMES)
+	@echo $^
 	$(LINK.cpp) -o $@ $(OUTPUT_OBJS) $(LDLIBS) 
 endif
 
